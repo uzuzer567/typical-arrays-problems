bf8c95190b644988df4721bb167258558c77dcd1
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-10 20:59:57 +0300

    feat: implementation of function min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0 ) {
+	return 0;
+  }
+  var answer = array[0];
+  for(var i = 0; i < array.length; i++) {
+	if(answer == undefined || array[i] == undefined) {
+		return 0;
+	}
+	else if(answer > array[i]) {
+		answer = array[i];
+	}
+  }
+  return answer;
 }
