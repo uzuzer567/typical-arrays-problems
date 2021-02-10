6a6c682b6132aea8f63822f199664caa97ab8b1c
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-10 21:00:31 +0300

    feat: implementation of function max

--- src/index.js
+++ src/index.js
@@ -13,3 +13,21 @@ exports.min = function min (array) {
   }
   return answer;
 }
+
+exports.max = function max (array) {
+  if(array == undefined || array.length == 0) {
+	return 0;
+  }
+  var answer = array[0];
+  for(var i = 0; i < array.length; i++) {
+	if(answer == undefined || array[i] == undefined) {
+		return 0;
+	}
+	else if(answer < array[i]) {
+		answer = array[i];
+	}
+  }
+  return answer;
+}
+
+
