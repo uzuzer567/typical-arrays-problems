7f1f68c075a92748ecf838a26f5183148ea7d2ff
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-10 21:01:04 +0300

    feat: implementation of function avg

--- src/index.js
+++ src/index.js
@@ -30,4 +30,18 @@ exports.max = function max (array) {
   return answer;
 }
 
-
+exports.avg = function avg (array) {
+  if(array == undefined || array.length == 0) {
+	return 0;
+  }
+  var answer = 0;
+  for(var i = 0; i < array.length; i++) {
+	if(answer == undefined || array[i] == undefined) {
+		return 0;
+	}
+	else {
+		answer += array[i];
+	}
+  }
+  return answer / array.length;
+}
